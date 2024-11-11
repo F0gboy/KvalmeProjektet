dc43238366c636f639f2ec279dcf21b2d0ed7596
leonardzz7 <leonard@leooli.dk>
2024-11-11 21:58:09 +0100

    MenuScripting

---
+++ Assets/SceneButtonScript.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneButtonScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField] string SceneName;
+    public void ChangeScene()
+    {
+        SceneManager.LoadScene(SceneName);
+    }
+
+}
